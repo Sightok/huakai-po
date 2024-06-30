817b497f3312a67b71a35aab3cd268863dc194a6
BuildTools <unconfigured@null.spigotmc.org>
2024-06-30 01:56:33 +0100

    Adding Huakai Gem Block

--- src/main/java/name/huakaipo/block/ModBlocks.java
+++ src/main/java/name/huakaipo/block/ModBlocks.java
@@ -5,12 +5,10 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.MapColor;
-import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
